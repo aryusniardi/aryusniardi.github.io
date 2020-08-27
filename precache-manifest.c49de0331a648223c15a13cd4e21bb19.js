126313b75169d93e9a86614605db69b82f0f38fc
Aryusniardi <aryusniardi15@gmail.com>
2020-08-27 08:50:06 +0700

    Updates

---
+++ precache-manifest.c49de0331a648223c15a13cd4e21bb19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984285c9dbdcb78ad74a72e74fe355ee",
+    "url": "/aryusniardi.github.io/index.html"
+  },
+  {
+    "revision": "4bd6ea42035f9a088a5c",
+    "url": "/aryusniardi.github.io/static/css/main.b6cef959.chunk.css"
+  },
+  {
+    "revision": "5d80cb3e04f724315ae8",
+    "url": "/aryusniardi.github.io/static/js/2.9fc2efd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aryusniardi.github.io/static/js/2.9fc2efd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd6ea42035f9a088a5c",
+    "url": "/aryusniardi.github.io/static/js/main.c6068a07.chunk.js"
+  },
+  {
+    "revision": "e6b4589445deda648277",
+    "url": "/aryusniardi.github.io/static/js/runtime-main.397756b4.js"
+  }
+]);
